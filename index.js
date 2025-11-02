cee8908f3d255935d879f176e63bdb835d4cf86e
WestOfBree <b.thomas1996@yahoo.com>
2025-11-02 16:16:32 -0500

    .map trouble

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 //http://www.omdbapi.com/?i=tt3896198&apikey=29e531e2
 let movie;
 const searchInput= document.querySelector(".search--input")
+const movieCard = document.querySelector(".movie-card")
 
 searchInput,addEventListener("input", element => {
     const value = element.target.value
     console.log(value)
 })
 async function render() {
-    const movieWrapper = document.querySelector(".results__container")
-    const movies = await fetch ("http://www.omdbapi.com/?i=tt3896198&apikey=29e531e2");
+    // const movieWrapper = document.querySelector(".results__container")
+    const movies = await fetch ("https://imdb.iamidiotareyoutoo.com/search?tt=tt2250912");
     const moviesData = await movies.json();
-    const movieCardHtml= moviesData.map((movie) => {
+    const movieCardHtml = moviesData.map((movie) => {
       return `<div class="movie">
                 <figure class="movie__img--wrapper">
                  <img class="movie__img"
@@ -26,8 +27,8 @@ async function render() {
     })
     .join("");
 
-    console.log(movieCardHtml)
-    movieWrapper.innerHTML = movieCardHtml;
+    console.log(moviesData)
+    movieCard.innerHTML = movieCardHtml;
 }
 render();
 setTimeout(() => {
